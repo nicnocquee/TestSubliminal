62ff2215d848d5745e6ee995b8ec8358435692d2
Nico Prananta <nicnocquee@yahoo.com>
2013-08-16 11:07:11 +0900

    initial

---
+++ TestSubliminal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestSubliminal
+//
+//  Created by Nico Prananta on 8/16/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestSubliminal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestSubliminal
+//
+//  Created by Nico Prananta on 8/16/13.
+//  Copyright (c) 2013 Touches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
